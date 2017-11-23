ea60d1c190a7bf5ba004eaa317b4ca3290e1d2d2
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-11-23 07:19:27 +0100

    fix to hide error

--- retarget.lua
+++ retarget.lua
@@ -1,3 +1,12 @@
+local error = true
+do
+	local orig = UIErrorsFrame_OnEvent
+	function UIErrorsFrame_OnEvent(event, msg)
+	    if error or msg ~= ERR_UNIT_NOT_FOUND then
+	        return orig(event, msg)
+	    end
+	end
+end
 local function feigning()
 	local i, buff = 1, nil
 	repeat
@@ -15,7 +24,9 @@ CreateFrame'Frame':SetScript('OnUpdate', function()
 	if target then
 		unit, dead, retarget = target, UnitIsDead'target', false
 	elseif unit then
+		error = false
 		TargetByName(unit, true)
+		error = true
 		if UnitExists'target' then
 			if not (retarget or (not dead and UnitIsDead'target' and feigning())) then
 				ClearTarget()
