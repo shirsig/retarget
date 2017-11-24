77b5aa3ffc1429b0f6843c7e328d42959fe1165d
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-11-24 07:34:26 +0100

    refactoring

--- retarget.lua
+++ retarget.lua
@@ -1,12 +1,3 @@
-local error = true
-do
-	local orig = UIErrorsFrame_OnEvent
-	function UIErrorsFrame_OnEvent(event, msg)
-	    if error or msg ~= ERR_UNIT_NOT_FOUND then
-	        return orig(event, msg)
-	    end
-	end
-end
 local function feigning()
 	local i, buff = 1, nil
 	repeat
@@ -19,13 +10,16 @@ local function feigning()
 	return UnitCanAttack('player', 'target')
 end
 local unit, retarget
+local pass = function() end
 CreateFrame'Frame':SetScript('OnUpdate', function()
 	local target = UnitName'target'
 	if target then
 		unit, dead, retarget = target, UnitIsDead'target', false
 	elseif unit then
-		error = false
+		local orig = UIErrorsFrame_OnEvent
+		UIErrorsFrame_OnEvent = pass
 		TargetByName(unit, true)
+		UIErrorsFrame_OnEvent = orig
 		error = true
 		if UnitExists'target' then
 			if not (retarget or (not dead and UnitIsDead'target' and feigning())) then
